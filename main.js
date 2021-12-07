b94b7e46ee10e6d4afc9ac1704b2cad79b0d4829
miraphero <mirarahman949@gmail.com>
2021-12-07 19:45:15 +0600

    second-time-commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const countDown = new Date ("Dec 6,2021 17:14:00").getTime();
+const countDown = new Date ("Dec 13,2021 17:14:00").getTime();
 
 const setTimeDisplay = setInterval(() => {
 
@@ -14,6 +14,28 @@ document.getElementById("minutes").textContent = minutes;
 
 const seconds = Math.floor(distance % (1000 * 60) / 1000);
 document.getElementById("seconds").textContent = seconds;
+
+
+}, 1000);
+
+// for Footer Section Timer
+
+const countDown2 = new Date ("Dec 14,2021 17:14:00").getTime();
+
+const setTimeDisplay2 = setInterval(() => {
+
+const nowTime2 = new Date().getTime();
+
+const distance2 = countDown2 - nowTime2;
+
+const hours2 = Math.floor(distance2 % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+document.getElementById("hours2").textContent = hours2;
+
+const minutes2 = Math.floor(distance2 % (1000 * 60 * 60) / (1000 * 60));
+document.getElementById("minutes2").textContent = minutes2;
+
+const seconds2 = Math.floor(distance2 % (1000 * 60) / 1000);
+document.getElementById("seconds2").textContent = seconds2;
 
 
 }, 1000);
